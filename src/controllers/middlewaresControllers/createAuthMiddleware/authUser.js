70583a4c7db3d5d21fbbfbee8e3d3e014e8fd3ee
Madhav <madhavm0706@gmail.com>
2025-07-09 19:42:05 +0530

    added authLogin

--- src/controllers/middlewaresControllers/createAuthMiddleware/authUser.js
+++ src/controllers/middlewaresControllers/createAuthMiddleware/authUser.js
@@ -4,56 +4,7 @@ const jwt = require('jsonwebtoken');
 const authUser = async (req, res, { user, databasePassword, password, UserPasswordModel }) => {
   const isMatch = await bcrypt.compare(databasePassword.salt + password, databasePassword.password);
 
-  if (!isMatch)
-    return res.status(403).json({
-      success: false,
-      result: null,
-      message: 'Invalid credentials.',
-    });
-
-  if (isMatch === true) {
-    const token = jwt.sign(
-      {
-        id: user._id,
-        companyId: user.companyId, 
-      },
-      process.env.JWT_SECRET,
-      { expiresIn: req.body.remember ? 365 * 24 + 'h' : '24h' }
-    );
-
-    await UserPasswordModel.findOneAndUpdate(
-      { user: user._id },
-      { $push: { loggedSessions: token } },
-      {
-        new: true,
-      }
-    ).exec();
-
-    res
-      .status(200)
-      .cookie('token', token, {
-        maxAge: req.body.remember ? 365 * 24 * 60 * 60 * 1000 : null,
-        sameSite: 'Lax',
-        httpOnly: true,
-        secure: false,
-        domain: req.hostname,
-        path: '/',
-        Partitioned: true,
-      })
-      .json({
-        success: true,
-        result: {
-          _id: user._id,
-          name: user.name,
-          surname: user.surname,
-          role: user.role,
-          email: user.email,
-          photo: user.photo,
-          permissions:user.permissions
-        },
-        message: 'Successfully login user',
-      });
-  } else {
+  if (!isMatch) {
     return res.status(403).json({
       success: false,
       result: null,
@@ -61,6 +12,42 @@ const authUser = async (req, res, { user, databasePassword, password, UserPasswo
     });
   }
 
+  const token = jwt.sign(
+    {
+      id: user._id,
+      companyId: user.companyId,
+    },
+    process.env.JWT_SECRET,
+    { expiresIn: req.body.remember ? '8760h' : '24h' } // 1 year or 1 day
+  );
+
+  await UserPasswordModel.findOneAndUpdate(
+    { user: user._id },
+    { $push: { loggedSessions: token } },
+    { new: true }
+  ).exec();
+
+  res
+    .status(200)
+    .cookie('token', token, {
+      httpOnly: true,
+      secure: true,           // must be true since Render uses HTTPS
+      sameSite: 'None',       // must be 'None' for cross-site cookies
+      maxAge: 24 * 60 * 60 * 1000, // optional
+    })
+    .json({
+      success: true,
+      result: {
+        _id: user._id,
+        name: user.name,
+        surname: user.surname,
+        role: user.role,
+        email: user.email,
+        photo: user.photo,
+        permissions: user.permissions,
+      },
+      message: 'Successfully login user',
+    });
 };
 
 module.exports = authUser;
